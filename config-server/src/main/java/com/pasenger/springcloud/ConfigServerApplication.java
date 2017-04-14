a09837e81e743c8fcded94a67185630c0cd9ba9f
Pasenger Hu <Pasenger Hu>
2017-04-15 00:34:20 +0800

    first commit

---
+++ config-server/src/main/java/com/pasenger/springcloud/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.pasenger.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Pasenger on 2017/4/15.
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
